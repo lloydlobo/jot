c6fb8001af39e12e743829eea67aca5c9c0aef83
Lloyd Lobo <76430758+lloydlobo@users.noreply.github.com>
2023-01-16 19:28:38 +0530

    feat: add git::commit method

--- jot/src/git.rs
+++ jot/src/git.rs
@@ -6,7 +6,7 @@ pub trait GitManagement {
     fn init(&mut self, repo_path: &str) -> Result<(), git2::Error>;
     fn checkout_branch(&mut self, branch_name: &str) -> Result<(), git2::Error>;
     fn add(&mut self) -> Result<(), git2::Error>;
-    fn commit(&mut self, subject: &str) -> Result<(), git2::Error>;
+    fn commit(&mut self, subject: &str) -> Result<git2::Oid, git2::Error>;
     fn push(&mut self, branch_name: &str) -> Result<(), git2::Error>;
 }
 
@@ -21,6 +21,8 @@ impl GitManagement for Git {
     ///
     /// The path can point to either a normal or bare repository.
     ///
+    /// Owned git repository, of all state associated with the underlying filesystem.
+    ///
     /// # Panics
     ///
     /// Panics if fails to open git repository with Error `code: -3`, `class: 2`, `message: "failed
@@ -28,21 +30,7 @@ impl GitManagement for Git {
     fn init(&mut self, repo_path: &str) -> Result<(), git2::Error> {
         let path: &Path = Path::new(&repo_path); // Directly wraps a string slice as a `Path` slice.
         let repository = git2::Repository::open(path);
-        dbg!(&repository?.path());
-
-        let path: &Path = Path::new(&repo_path); // Directly wraps a string slice as a `Path` slice.
-        let repository = git2::Repository::open(path);
-        match repository {
-            Ok(repo) => {
-                dbg!(&repo.path());
-                self.repo = Some(repo);
-                Ok(())
-            }
-            Err(err) => Err(err),
-        }
-
-        // Owned git repository, of all state associated with the underlying filesystem.
-        // repository.map(|repo| self.repo = Some(repo))
+        repository.map(|repo| self.repo = Some(repo))
     }
 
     /// Updates files in the index and working tree to match the content of the
@@ -88,17 +76,57 @@ impl GitManagement for Git {
         index.write()
     }
 
-    fn commit(&mut self, subject: &str) -> Result<(), git2::Error> {
-        todo!()
+    /// Get the Index file for this repository.
+    ///
+    /// If a custom index has not been set, the default index for the repository
+    /// will be returned (the one located in .git/index).
+    ///
+    /// Create a new action signature with default user and now timestamp.
+    fn commit(&mut self, subject: &str) -> Result<git2::Oid, git2::Error> {
+        let repo: &git2::Repository = self.repo.as_ref().unwrap();
+        let mut index: git2::Index = repo.index()?;
+
+        let signature: git2::Signature = repo.signature()?; // Use default user.name and user.email
+
+        let oid: git2::Oid = index.write_tree()?; // Unique identity of any object (commit, tree, blob, tag).
+        let parent_commit: git2::Commit = find_last_commit(self.repo.as_ref().unwrap())?; // A structure to represent a git [commit][1]
+        let tree: git2::Tree = repo.find_tree(oid)?; // A structure to represent a git [tree][1]
+
+        repo.commit(
+            Some("HEAD"),      // Point HEAD to our new commit
+            &signature,        // Author
+            &signature,        // Committer
+            subject,           // Commit message
+            &tree,             // Tree
+            &[&parent_commit], // Parent Commit
+        )
     }
 
+    /*
+    TODO:
+     >> Jot summary
+    > Test 1
+    Git initialized successfully
+    [jot/src/lib.rs:169] &t = ()
+    Adding and committing you new jot to main..
+    Added and committed:
+    Pushing your new jot...
+    Pushed! */
     fn push(&mut self, branch_name: &str) -> Result<(), git2::Error> {
+        // Ok(())
         todo!()
     }
 }
 
-fn find_last_commint(repo: &git2::Repository) -> Result<git2::Commit, git2::Error> {
-    todo!()
+/// * `head` - Retrieve and resolve the reference pointed at by HEAD.
+/// * `resolve` - Resolve a symbolic reference to a direct reference. This method iteratively peels
+///   a symbolic reference until it resolves to a direct reference to an OID.
+/// * `peel` - Peel a reference to an object - This method recursively peels the reference until it
+///   reaches an object of the specified type.
+///     * `git2::ObjectType::Commit` An object which corresponds to a git commit.
+fn find_last_commit(repo: &git2::Repository) -> Result<git2::Commit, git2::Error> {
+    let obj: git2::Object = repo.head()?.resolve()?.peel(git2::ObjectType::Commit)?;
+    obj.into_commit().map_err(|_| git2::Error::from_str("Couldn't find commit"))
 }
 
 fn with_credentials<F>(repo: &git2::Repository, mut f: F) -> Result<(), git2::Error>
@@ -106,4 +134,5 @@ where
     F: FnMut(&mut git2::Credentials) -> Result<(), git2::Error>,
 {
     todo!()
+    // Ok(())
 }
